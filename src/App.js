4c70ef93e7161fef461ab82bd8d69736c83ccb5f
harpreet <Harpreet Kour>
2020-07-30 11:20:32 -0600

    made change in master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
      <h1> Git Branching / Merge Practice</h1>
-     <p>Let's practice Git Branching and Merging!</p>
+     <p>Let's practice some Git Branching and Merging!</p>
      <About />
      <Services />
     </div>
